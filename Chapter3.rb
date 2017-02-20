b2fc3e7b22514080e61076eb11f2bd34364f9aef
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-02-20 11:09:44 +0000

    Chapter3

---
+++ Chapter3.rb
@@ -0,0 +1,13 @@
+name = 'Catherine Mary Gleeson'
+puts 'My name is ' + name + '.'
+name = 'Sylvia Rose Aurora Gleeson'
+puts 'My niece is ' + name + '.'
+
+var = 'this' + 'that'
+puts var
+
+var = 12*12
+puts var
+
+var = name
+puts var 
