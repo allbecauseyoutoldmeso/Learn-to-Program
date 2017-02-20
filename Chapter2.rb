d96fd0755e7e7c5675c2656d31d647f2e87ed5d0
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-02-20 11:04:48 +0000

    Chapter2

---
+++ Chapter2.rb
@@ -0,0 +1,12 @@
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'blink' * 4
+puts '2+2'
+puts '2' + '2'
+puts 2+2
+puts 2*2
+puts '2'*2
+puts 'That\'s cool.'
+puts 'Black\\slash.'
+puts 'Backs\lash.'
+puts 'Backslash: \\'
