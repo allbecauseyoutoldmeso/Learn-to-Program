f2ebdcd008c958a8e09b9142570dfcc13ec93f20
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-02-20 12:19:52 +0000

    Chapter5/3

---
+++ Chapter5/HigherMath.rb
@@ -0,0 +1,24 @@
+puts 5**2
+puts 5**0.5
+puts 7/3
+puts 7%3
+puts 1000%6
+
+puts 1000.0%6.0
+
+puts (10-20).abs
+
+puts rand
+puts rand(100)
+puts rand(1)
+
+srand 1981
+puts rand(10)
+puts rand(10)
+srand 1981
+puts rand(10)
+puts rand(10)
+
+puts Math::PI
+puts Math.cos(20)
+puts Math::E
